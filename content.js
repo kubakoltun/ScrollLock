2f3385feede24516dc35c31e2a02a0a594a333a9
kubakoltun <kubakoltun1232@gmail.com>
2024-07-04 22:52:51 +0200

    fix: verification of the hostnames moved completely to the scroll locking file

--- content.js
+++ content.js
@@ -1,8 +1,29 @@
 function disableScroll() {
   window.addEventListener('scroll', function(event) {
-    event.preventDefault();
-    window.scrollTo(0, 0);
+      event.preventDefault();
+      window.scrollTo(0, 0);
   }, { passive: false });
 }
 
-disableScroll();
+async function init() {
+  const pages = await getPagesFromStorage();
+  const currentHost = window.location.hostname;
+
+  if (pages.includes(currentHost)) {
+      disableScroll();
+  }
+}
+
+function getPagesFromStorage() {
+  return new Promise((resolve, reject) => {
+      try {
+          chrome.storage.local.get(['scrollLockPages'], function(result) {
+              resolve(result.scrollLockPages || []);
+          });
+      } catch (error) {
+          reject(error);
+      }
+  });
+}
+
+init();
