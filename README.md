3b6bb2b7ceb4db5dac01ff7e66f9f693bd03aec9
kubakoltun <kubakoltun1232@gmail.com>
2024-10-06 12:39:02 +0200

    refa: no default websites

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 ScrollLock is a simple Google Chrome plugin that locks a scroll on desired pages.
-
-There are some default pages already entered.
--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 chrome.runtime.onInstalled.addListener(function(details) {
-  const defaultSites = ['www.facebook.com', 'www.youtube.com', 'www.instagram.com', 'www.tiktok.com'];
+  const defaultSites = [];
 
-  chrome.storage.local.get(['scrollLockPages'], function(result) {
-    if (!result.scrollLockPages || result.scrollLockPages.length === 0 || details.reason === "install") {
-      chrome.storage.local.set({ scrollLockPages: defaultSites }, function() {
-        console.log('Default sites set:', defaultSites);
-      });
-    }
-  });
+  // chrome.storage.local.get(['scrollLockPages'], function(result) {
+  //   if (!result.scrollLockPages || result.scrollLockPages.length === 0 || details.reason === "install") {
+  //     chrome.storage.local.set({ scrollLockPages: defaultSites }, function() {
+  //       console.log('Default sites set:', defaultSites);
+  //     });
+  //   }
+  // });
 });
