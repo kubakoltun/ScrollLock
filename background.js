ab102d6137e9341dd2028b414ca94c80ef61873c
kubakoltun <kubakoltun1232@gmail.com>
2025-11-08 12:13:31 +0100

    refa: indentions changes to 4, added simple logo, removed unused permissions

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 chrome.runtime.onInstalled.addListener(function(details) {
-  const defaultSites = [];
+    const defaultSites = [];
 
-  // chrome.storage.local.get(['scrollLockPages'], function(result) {
-  //   if (!result.scrollLockPages || result.scrollLockPages.length === 0 || details.reason === "install") {
-  //     chrome.storage.local.set({ scrollLockPages: defaultSites }, function() {
-  //       console.log('Default sites set:', defaultSites);
-  //     });
-  //   }
-  // });
+    // chrome.storage.local.get(['scrollLockPages'], function(result) {
+    //   if (!result.scrollLockPages || result.scrollLockPages.length === 0 || details.reason === "install") {
+    //     chrome.storage.local.set({ scrollLockPages: defaultSites }, function() {
+    //       console.log('Default sites set:', defaultSites);
+    //     });
+    //   }
+    // });
 });
--- content.js
+++ content.js
@@ -1,38 +1,38 @@
 function preventDefault(e) {
-  e.preventDefault();
+    e.preventDefault();
 }
 
 function disableScroll() {
-  // Retrying if body is not available yet
-  if (!document.body) {
-    setTimeout(disableScroll, 100);
-    return;
-  }
+    // Retrying if body is not available yet
+    if (!document.body) {
+        setTimeout(disableScroll, 100);
+        return;
+    }
 
-  document.body.style.overflow = 'hidden';
-  document.addEventListener('touchmove', preventDefault, { passive: false });
-  document.addEventListener('wheel', preventDefault, { passive: false });
+    document.body.style.overflow = 'hidden';
+    document.addEventListener('touchmove', preventDefault, { passive: false });
+    document.addEventListener('wheel', preventDefault, { passive: false });
 }
 
 async function init() {
-  const pages = await getPagesFromStorage();
-  const currentHost = window.location.hostname;
+    const pages = await getPagesFromStorage();
+    const currentHost = window.location.hostname;
 
-  if (pages.includes(currentHost)) {
-    disableScroll();
-  }
+    if (pages.includes(currentHost)) {
+        disableScroll();
+    }
 }
 
 function getPagesFromStorage() {
-  return new Promise((resolve, reject) => {
-    try {
-      chrome.storage.local.get(['scrollLockPages'], function(result) {
-        resolve(result.scrollLockPages || []);
-      });
-    } catch (error) {
-      reject(error);
-    }
-  });
+    return new Promise((resolve, reject) => {
+        try {
+            chrome.storage.local.get(['scrollLockPages'], function(result) {
+                resolve(result.scrollLockPages || []);
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
 }
 
 init();
